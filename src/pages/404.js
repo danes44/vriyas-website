1a063e8c6772e6713dced8b18fb519aade925409
Vriyas Hartama <vriyashartama@gmail.com>
2018-11-05 09:52:12 +0700

    Moves to bulma

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Layout from '../components/layout'
+import "../styles/index.scss"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section className="section">
+      <div className="container">
+        <h1 className="title 1">NOT FOUND</h1>
+        <p>Ouch, The page you were looking for doesn't exist. You may have mistyped the address or the page may have moved.</p>
+      </div>
+    </section>
   </Layout>
 )
 
